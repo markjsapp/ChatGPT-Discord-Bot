ee572175783dbe5c9d43426903157080ea20230c
Mark <markjsapp@gmail.com>
2023-03-13 22:05:31 -0400

    update bot to use OpenAI's API and remove webhook logic, update README accordingly

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
-import discord
-from discord import Intents
-from discord.ext import commands
-import requests
-import json
-import os
 from dotenv import load_dotenv
+from discord.ext import commands
+import discord
+import openai
+import time
 
-# Set the webhook URL
-webhook_url = 'https://hook.us1.make.com/lw7k8jkk1deqer77zcq9s6kvf1r7941c'
+# keep track of how many times we've asked a question
+question_count = 0
 
-# create a new bot instance
+# create a new discord bot instance
 intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+openai.api_key = "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"
+
 # define a command that sends a message when triggered
 @bot.command(name='ask')
-async def askChatGPT(ctx, question):
-    await ctx.send(f"{question.author} asked ChatGPT: {question}")
+async def askChatGPT(ctx):
+    # count stuff
+    global question_count
+    question_count += 1
+    # parse for the user's question 
+    userMessage = ctx.message.content
+    # strip away the !ask text
+    question = userMessage[5:]
+    # print the question asked to the channel 
+    await ctx.send(f"{ctx.author} asked ChatGPT: \n```{question}```")
 
-    payload = {
-    "question": question
-    }
+    # create a request
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": question }]
+    )
 
-    # Make the HTTP request
-    response = requests.post(webhook_url, json=payload)
+    # timeout may vary due to huge responses
+    time.sleep(10)
 
-    if response.status_code == 200:
-        await ctx.send("Question was sent successfully! Waiting on response...")
-    else:
-        await ctx.send("Webhook POST request failed...")
+    response = response.choices[0].message.content
+    await ctx.send(f"ChatGPT said: \n```{response}```")
 
-# run the bot
-bot.run('MTA4NDYwODQxNDkyMTAwMzA5OA.G0UywQ.t52Ws8jfXHM1EMwDYPYz7o0gJa9Dz98SSh4F_A')
+@bot.command(name='count')
+async def countMethod(ctx):
+    await ctx.send(f"{count}")
+
+def main():
+     # load environment variables
+    load_dotenv()
+    # run the bot
+    bot.run('MTA4NDYwODQxNDkyMTAwMzA5OA.G0UywQ.t52Ws8jfXHM1EMwDYPYz7o0gJa9Dz98SSh4F_A')
+
+if __name__ == '__main__':
+    main()
