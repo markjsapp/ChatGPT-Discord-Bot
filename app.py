0dc2ead0003f23d032e02d7c23d47662cdcb67bf
Mark <markjsapp@gmail.com>
2023-03-13 22:37:06 -0400

    update bot to use threading when multiple users ask questions in a short amount of time.  Remove credentials and regenerate new ones

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from dotenv import load_dotenv
 from discord.ext import commands
+from dotenv import load_dotenv
 import discord
 import openai
+import threading
 import time
 
 # keep track of how many times we've asked a question
 question_count = 0
+
+# for threading
+last_response = {}
 
 # create a new discord bot instance
 intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-openai.api_key = "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"
+openai.api_key = {YOUR_OPENAI_API_TOKEN}
 
-# define a command that sends a message when triggered
+# sends a message 
 @bot.command(name='ask')
 async def askChatGPT(ctx):
     # count stuff
@@ -27,28 +31,53 @@ async def askChatGPT(ctx):
     # print the question asked to the channel 
     await ctx.send(f"{ctx.author} asked ChatGPT: \n```{question}```")
 
-    # create a request
-    response = openai.ChatCompletion.create(
+    # use threading to handle each user's question
+    thread = threading.Thread(target=handle_question, args=(ctx.author.id, question, ctx.channel.id))
+    thread.start()
+
+# method that 
+@bot.command(name='count')
+async def countMethod(ctx):
+    await ctx.send(f"{question_count}")
+
+def handle_question(user_id, question, channel_id):
+    global last_response
+    try:
+        # check if this user has asked a question before
+        if user_id in last_response:
+            # check if the previous response was less than 10 seconds ago
+            if time.time() - last_response[user_id]['timestamp'] < 10:
+                # send the cached response instead of making a new request
+                response = last_response[user_id]['response']
+                send_response(response, channel_id)
+                return
+        # make a new request
+        response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
         {"role": "system", "content": question }]
-    )
+        )
 
-    # timeout may vary due to huge responses
-    time.sleep(10)
+        time.sleep(10)
+        
+        actual_response = response.choices[0].message.content
 
-    response = response.choices[0].message.content
-    await ctx.send(f"ChatGPT said: \n```{response}```")
+        # cache the response for this user
+        last_response[user_id] = {'response': actual_response, 'timestamp': time.time()}
+        send_response(actual_response, channel_id)
+    except Exception as e:
+        # handle errors
+        print(f"Error handling question from user {user_id}: {e}")
 
-@bot.command(name='count')
-async def countMethod(ctx):
-    await ctx.send(f"{count}")
+def send_response(response, channel_id):
+    # send the response to the channel
+    bot.loop.create_task(bot.get_channel(channel_id).send(f"ChatGPT said: \n```{response}```"))
 
 def main():
      # load environment variables
     load_dotenv()
     # run the bot
-    bot.run('MTA4NDYwODQxNDkyMTAwMzA5OA.G0UywQ.t52Ws8jfXHM1EMwDYPYz7o0gJa9Dz98SSh4F_A')
+    bot.run({YOUR_DISCORD_BOT_TOKEN})
 
 if __name__ == '__main__':
     main()
